58939c88cf60185fed6b94cedd52362639380af9
BEPISMAN2 <helpithinkihavehermes@gmail.com>
2017-11-10 09:40:11 -0500

    added full button control, tweaked UI

--- source/ui.c
+++ source/ui.c
@@ -8,6 +8,7 @@
 #define WHITE   RGBA8(255, 255, 255, 255)
 #define GREYBG  RGBA8(255, 120, 120, 120)
 #define LGREYBG RGBA8(255, 200, 200, 200)
+#define LGREYFG RGBA8(200, 200, 200, 255)
 #define GREYFG  RGBA8(120, 120, 120, 255)
 #define GREY2FG RGBA8(80,   80,  80, 255)
 #define DGREY   RGBA8( 50,  50,  50, 255)
@@ -35,6 +36,7 @@ u8* modSelected;
 s16 entryIndex = 0;  // position of index in array
 u16 indexPos = 0;    // position of screen in array
 u8 cursorPos = 0;    // is the cursor in the file browser or the button list?
+s16 buttonIndex = 0;
 
 // END
 // ------------------------------------------------------
@@ -91,10 +93,16 @@ void createButton(UIButton* but, int x, int y, int width, int height, int textIn
     }
 }
 
-void drawButton(UIButton* but) {
-    int textWidth = pp2d_get_text_width(but->text, 0.4f, 0.4f);
+void drawButton(UIButton* but, bool selected) {
+    u32 color;
     
-    pp2d_draw_rectangle(but->x, but->y, but->width, but->height, GREYFG);
+    int textWidth = pp2d_get_text_width(but->text, 0.4f, 0.4f);
+    if (selected)
+        color = LGREYFG;
+    else
+        color = GREYFG;
+    
+    pp2d_draw_rectangle(but->x, but->y, but->width, but->height, color);
     pp2d_draw_text(but->x + (but->width / 2) - (textWidth / 2), but->y + (but->height / 3), 0.4f, 0.4f, WHITE, but->text);
 }
 
@@ -182,17 +190,74 @@ LOOP_RETURN uiModSelectLoop() {
             break;
         
         // button input
-        switch (kDown) {
-            case KEY_UP:
-                entryIndex--;
-                break;
-            case KEY_DOWN:
-                entryIndex++;
-                add = 1;
-                break;
-            default:
-                break;
-        };
+        if (cursorPos == 0) {
+            switch (kDown) {
+                case KEY_UP:
+                    entryIndex--;
+                    break;
+                case KEY_DOWN:
+                    entryIndex++;
+                    add = 1;
+                    break;
+                case KEY_LEFT:
+                    cursorPos = !cursorPos;
+                    break;
+                case KEY_RIGHT:
+                    cursorPos = !cursorPos;
+                    break;
+                default:
+                    break;
+            };
+            
+            if (entryIndex >= modCount) {
+                entryIndex = 0;
+                indexPos = 0;
+            } else if (entryIndex < 0) {
+                entryIndex = modCount - 1;
+                indexPos = ((int)ceil((float)modCount / 13.0) - 1) * 13;
+            }
+        
+            if (entryIndex < indexPos)
+                indexPos = indexPos - 13;
+            else if ((entryIndex % 13) == 0 && entryIndex != 0 && add == 1)
+                indexPos = indexPos + 13;
+        } else if (cursorPos == 1) {
+            switch (kDown) {
+                case KEY_UP:
+                    if (buttonIndex == 7)
+                        buttonIndex--;
+                    buttonIndex--;
+                    break;
+                case KEY_DOWN:
+                    if (buttonIndex == 6)
+                        buttonIndex++;
+                    buttonIndex++;
+                    break;
+                case KEY_LEFT:
+                    if (buttonIndex == 7)
+                        buttonIndex--;
+                    else if (buttonIndex == 6)
+                        buttonIndex++;
+                    else
+                        cursorPos = !cursorPos;
+                    break;
+                case KEY_RIGHT:
+                    if (buttonIndex == 6)
+                        buttonIndex++;
+                    else if (buttonIndex == 7)
+                        buttonIndex--;
+                    else
+                        cursorPos = !cursorPos;
+                    break;
+                default:
+                    break;
+            };
+            
+            if (buttonIndex >= NUMBTNS)
+                buttonIndex = 0;
+            else if (buttonIndex < 0)
+                buttonIndex = NUMBTNS - 1;
+        }
         
         // touch input
         s16 buttonTouched = getCurrentButtonTouched(&touch);
@@ -226,20 +291,7 @@ LOOP_RETURN uiModSelectLoop() {
             }
         }        
         
-        if (entryIndex >= modCount) {
-            entryIndex = 0;
-            indexPos = 0;
-        } else if (entryIndex < 0) {
-            entryIndex = modCount - 1;
-            indexPos = ((int)ceil((float)modCount / 13.0) - 1) * 13;
-        }
-        
-        if (kDown) {
-            if (entryIndex < indexPos)
-                indexPos = indexPos - 13;
-            else if ((entryIndex % 13) == 0 && entryIndex != 0 && add == 1)
-                indexPos = indexPos + 13;
-        }
+
         
         // display to screen
         pp2d_begin_draw(GFX_BOTTOM);
@@ -247,7 +299,7 @@ LOOP_RETURN uiModSelectLoop() {
             int max = (indexPos + 13 < modCount) ? indexPos + 13 : modCount;
             
             for (int i = indexPos; i < max; i++) {
-                if (i == entryIndex)
+                if (i == entryIndex && cursorPos == 0)
                     pp2d_draw_rectangle(0, 20 + ((i % 13) * 15), 320, 15, GREYFG);
                 pp2d_draw_text(0, 20 + ((i % 13) * 15), 0.5f, 0.5f, WHITE, modListing[i]);
                 pp2d_draw_text(50, 20 + ((i % 13) * 15), 0.5f, 0.5f, WHITE, "aaaabbbb");
@@ -260,7 +312,7 @@ LOOP_RETURN uiModSelectLoop() {
         
             // draw all the side buttons
             for (int i = 0; i < NUMBTNS; i++) {
-                drawButton(buttonList[i]);
+                drawButton(buttonList[i], (i == buttonIndex && cursorPos == 1) || isButtonTouched(buttonList[i], &touch));
             }
         pp2d_end_draw();
     }
